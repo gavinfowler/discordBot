c9391e82c471dff8901074a3025ff88811ef0535
Gavin Fowler <gavins.username@gmail.com>
2019-08-09 15:27:44 -0600

    Fix connection

--- bot.js
+++ bot.js
@@ -7,13 +7,33 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function smdhoes(msg, channel) {
+	if (!channel) return console.error("The channel does not exist!");
+	if (msg.content == '++smdhoes') {
+		channel.join().then(connection => {
+			let dispatcher = connection.playFile('./audio.mp3')
+			// this is not working well on heroku.
+			// think about using commands to connect and disconnect
+			dispatcher.on('speaking', (value) => {
+				if (value == false)
+					connection.disconnect()
+			})
+		}).catch(e => {
+			console.error(e);
+		});
+	}
+}
+
 client.on('message', msg => {
 	if (msg.author.bot == false) {
 		var message = msg.content
 		var beginning = message.substring(0, 2)
+		if (beginning != "++") {
+			return;
+		}
 		var command = message.substring(2, message.indexOf(' '))
 		var args = message.substring(message.indexOf(' '))
-		if(command == beginning){
+		if (command == beginning) {
 			command = message.substring(2, message.length)
 			args = null
 		}
@@ -22,36 +42,23 @@ client.on('message', msg => {
 		console.log(`beginning: ${beginning}`)
 		console.log(`Command: ${command}`)
 		console.log(`args: ${args}`)
-		if (beginning == "++") {
-			// console.log(msg.channel)
-			// msg.channel.send('hello!')
-			// 	.then(message => console.log(`Sent message: ${message.content}`))
-			// 	.catch(console.error);
-			switch (command) {
-				case 'repeat':
-					msg.channel.send(args)
-						.catch(console.error);
-					break;
-				case 'connect':
-					const channel = client.channels.get(msg.member.voiceChannelID);
-					if (!channel) return console.error("The channel does not exist!");
-					if (msg.content == '++smdhoes') {
-						channel.join().then(connection => {
-							let dispatcher = connection.playFile('./audio.mp3')
-							// this is not working well on heroku.
-							// think about using commands to connect and disconnect
-							dispatcher.on('speaking', (value) => {
-								if (value == false)
-									connection.disconnect()
-							})
-						}).catch(e => {
-							console.error(e);
-						});
-					}
-				default:
-					msg.channel.send('Unknown command.')
-						.catch(console.error);
-			}
+		// console.log(msg.channel)
+		// msg.channel.send('hello!')
+		// 	.then(message => console.log(`Sent message: ${message.content}`))
+		// 	.catch(console.error);
+		switch (command) {
+			case 'repeat':
+				msg.channel.send(args)
+					.catch(console.error);
+				break;
+			case 'connect':
+				const channel = client.channels.get(msg.member.voiceChannelID);
+				console.log('connecting')
+				smdhoes(msg, channel);
+				break;
+			default:
+				msg.channel.send('Unknown command.')
+					.catch(console.error);
 		}
 	}
 });
